f13de8d483089d133b769482f1e2698e5218f15c
komed3 <pk97@outlook.de>
2025-09-02 13:44:33 +0200

    i18n setup

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@
 import express from 'express';
 
 import { CFG } from './includes/config';
+import { i18n_setup } from './includes/i18n';
 
 const app = express();
+
+i18n_setup( app );
 
 app.listen( CFG.server.port );
---
+++ src/includes/i18n.ts
@@ -0,0 +1,41 @@
+'use strict';
+
+import type { Express } from 'express';
+
+import { join } from 'path';
+
+import i18next from 'i18next';
+import { LanguageDetector, handle } from 'i18next-http-middleware';
+import Backend from 'i18next-fs-backend';
+
+import { DIR, CFG } from './config';
+
+function i18n_setup ( app: Express ) : void {
+
+    const { fallback = 'en', preload = [ 'en' ], https = false } = CFG.i18n;
+
+    i18next
+        .use( Backend )
+        .use( LanguageDetector )
+        .init( {
+            fallbackLng: fallback,
+            preload: preload,
+            defaultNS: 'generic',
+            nonExplicitSupportedLngs: true,
+            backend: {
+                loadPath: join( DIR, 'locales/{{lng}}/{{ns}}.json' )
+            },
+            detection: {
+                order: [ 'cookie', 'header' ],
+                lookupCookie: 'locale',
+                caches: [ 'cookie' ],
+                cookieSameSite: "strict",
+                cookieSecure: https
+            }
+        } );
+
+    app.use( handle( i18next ) );
+
+};
+
+export { i18n_setup };
