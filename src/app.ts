20559c7c760e56f8cdcd6cf387c4e3e996740727
komed3 <pk97@outlook.de>
2025-09-02 15:10:43 +0200

    setup pug rendering

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 'use strict';
 
 import express from 'express';
+import { join } from 'path';
 
-import { CFG } from './includes/config';
+import { DIR, CFG } from './includes/config';
 import { i18n_setup } from './includes/i18n';
+import { routes } from './includes/routing';
+
+const { port = 3000 } = CFG.server;
 
 const app = express();
 
 i18n_setup( app );
 
-app.listen( CFG.server.port );
+app.set( 'views', join( DIR, 'templates' ) );
+app.set( 'view engine', 'pug' );
+
+for ( const route of routes ) {
+    ( app as any )[ route.method ]( route.path, route.handler );
+}
+
+app.listen( port );
