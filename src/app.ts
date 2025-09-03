2b2e43326041b1d4100c8f2c66bfdcaa30bc4638
komed3 <pk97@outlook.de>
2025-09-03 13:29:01 +0200

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { loadConfig } from '@config/index';
+
+async function bootstrap () : Promise<void> {
+
+    try {
+
+        const config = await loadConfig();
+
+    } catch ( err ) {
+
+        console.error( `Failed to start server:`, err );
+        process.exit( 1 );
+
+    }
+
+}
+
+bootstrap();
