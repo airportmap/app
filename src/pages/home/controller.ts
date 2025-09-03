e0cf0ffd55d1a4c662fcd3f8d183ee7005f2fd8e
komed3 <pk97@outlook.de>
2025-09-03 16:59:17 +0200

    cleanup

--- src/pages/home/controller.ts
+++
@@ -1,7 +0,0 @@
-import { type Request, type Response } from 'express';
-
-export default function homeController ( req: Request, res: Response ) : void {
-
-    res.status( 200 ).render( 'home/views/index', {} );
-
-}
