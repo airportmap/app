180a4a36a9be8c3c1829bdea9ee4ed15f1e34714
komed3 <pk97@outlook.de>
2025-09-04 13:05:28 +0200

    set up static paths

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,10 +1,16 @@
 import { type AppConfig } from '@core/config';
 import { setupI18n } from '@core/i18n';
-import express, { type Application } from 'express';
+import express, { static as static_, type Application } from 'express';
 
 export async function createServer ( cfg: AppConfig ) : Promise< Application > {
 
+    const { assets, js, css } = cfg.paths;
+
     const app = express();
+
+    app.use( '/assets', static_( assets ) );
+    app.use( '/js', static_( js ) );
+    app.use( '/css', static_( css ) );
 
     await setupI18n( app, cfg );
 
