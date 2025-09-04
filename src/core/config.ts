e7ed5d2d0223b746d1b9f51e166fafed7d3fd8ce
komed3 <pk97@outlook.de>
2025-09-04 12:37:02 +0200

    i18n config

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,1 +1,10 @@
-export interface AppConfig {}
+export interface I18nConfig {
+    pattern: string;
+    fallbackLng: string;
+    supportedLngs: string[];
+    namespaces: string[];
+}
+
+export interface AppConfig {
+    i18n: I18nConfig;
+}
--- src/core/i18n.ts
+++ src/core/i18n.ts
@@ -6,10 +6,26 @@ import { LanguageDetector, handle } from 'i18next-http-middleware';
 
 export async function setupI18n ( app: Application, cfg: AppConfig ) : Promise< void > {
 
+    const { pattern, fallbackLng, supportedLngs, namespaces } = cfg.i18n;
+
     await i18next
         .use( FsBackend )
         .use( LanguageDetector )
-        .init( {} );
+        .init( {
+            fallbackLng: fallbackLng,
+            supportedLngs: supportedLngs,
+            ns: namespaces,
+            defaultNS: namespaces[ 0 ],
+            backend: {
+                loadPath: pattern
+            },
+            detection: {
+                order: [ 'cookie', 'header' ],
+                lookupCookie: 'locale',
+                caches: [ 'cookie' ],
+                cookieSameSite: 'strict'
+            }
+        } );
 
     app.use( handle( i18next ) );
 
