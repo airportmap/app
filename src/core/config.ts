2a32228990f7c9e7357c7e02733bbecd8e76db58
komed3 <pk97@outlook.de>
2025-09-04 14:47:33 +0200

    load config

--- src/core/config.ts
+++ src/core/config.ts
@@ -1,12 +1,36 @@
 import { type AppConfig } from '@types';
+import { all } from 'deepmerge';
+import { readFile } from 'fs/promises';
+import { load } from 'js-yaml';
 import { join } from 'path';
 
 export const ENV: string = process.env.NODE_ENV || 'production';
-
 export const PATH: string = join( __dirname, '../..' );
+
+async function loadCfgFile ( filePath: string ) : Promise< object > {
+
+    try {
+
+        const content = await readFile( filePath, 'utf8' );
+        return load( content ) as object;
+
+    } catch ( err ) {
+
+        console.warn( `Cannot load config file ${ filePath }`, err.message );
+
+    }
+
+    return {};
+
+}
 
 export async function loadConfig () : Promise< AppConfig > {
 
-    return {} as AppConfig;
+    return all( [
+        await loadCfgFile( join( PATH, `conf/default.yml` ) ),
+        await loadCfgFile( join( PATH, `conf/env/${ ENV }.yml` ) ),
+        await loadCfgFile( join( PATH, `i18n/i18n.config.yml` ) ),
+        await loadCfgFile( join( PATH, `modules/modules.config.yml` ) )
+    ] ) as AppConfig;
 
 }
